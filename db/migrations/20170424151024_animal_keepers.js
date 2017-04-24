107b027a883068da3054645b47672841da08b944
Sorrel Brigman <sorrel.k@gmail.com>
2017-04-24 15:22:37 -0500

    changed keeper to keepers

--- db/migrations/20170424151024_animal_keepers.js
+++ db/migrations/20170424151024_animal_keepers.js
@@ -2,7 +2,7 @@
 exports.up = function(knex, Promise) {
   knex.schema.table('animal_keeper', function(table){
     table.integer('animal_id').unsigned().notNullable().references('animals.id');
-    table.integer('keeper_id').unsigned().notNullable().references('keeper.id');
+    table.integer('keeper_id').unsigned().notNullable().references('keepers.id');
 };
 
 exports.down = function(knex, Promise) {
