8e077c11d810f2dbec84bcb3284733b389c2dca4
Wisbell <westleyisbell@gmail.com>
2017-04-24 13:52:45 -0500

    Created seed file and updated keepers.json

---
+++ db/seeds/development/b_keeper_seeds.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const keepers = require('../data/keepers');
+
+console.log('keepers', keepers);
+
+exports.seed = function(knex, Promise) {
+
+  const keeperPromises = keepers.map( ({first_name, last_name, gender, age, date_of_hire, speciality, keeper_image}) => {
+    return knex('keepers').insert({first_name, last_name, gender, age, date_of_hire, speciality, keeper_image})
+  })
+
+  return knex('keepers').del() // Deletes ALL existing entries
+    .then(function () { // Inserts seed entries one by one in series
+      return Promise.all(keeperPromises);
+};
