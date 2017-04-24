fd17ba9bfb582843a73f0fbdba8d7ce382f8e67d
Jared Fuller <jaredshane@gmail.com>
2017-04-24 15:18:55 -0500

    added animals migration

---
+++ db/migrations/20170424151016_animals.js
@@ -0,0 +1,11 @@
+exports.up = function(knex, Promise) {
+ knex.schema.table('animals', function(table){
+   table.string('name').notNullable();
+   table.string('species').notNullable();
+   table.string('age').notNullable();
+   table.string('gender').notNullable();
+   table.integer('category_id').notNullable().unsigned().references('directors.id');
+   table.integer('number_of_kills').notNullable();
+   table.string('animal_image').notNullable();
+   })
+ };
