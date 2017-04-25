fbeff47a0981bdbe5a8d1c62c83c1e8870d8246e
Jared Fuller <jaredshane@gmail.com>
2017-04-25 15:40:10 -0500

    required in cascade delete, thank you Lucas, Jufe, and Priya

--- db/database.js
+++ db/database.js
@@ -2,7 +2,10 @@ const environment = process.env.NODE_ENV  || 'development';
 const config = require('../knexfile.js')[environment];
 const knex = require('knex')(config);
 const bookshelf = require('bookshelf')(knex);
+const cascadeDelete = require('bookshelf-cascade-delete');
+
 
 bookshelf.plugin('registry');
+bookshelf.plugin(cascadeDelete);
 
 module.exports = {knex, bookshelf};
