4cef5a336caa135314108e00c07bd299a2ae1921
Jared Fuller <jaredshane@gmail.com>
2017-04-25 11:40:11 -0500

    wrote function in controller to call getOneAnimal function, pass it the id from the route params and return it with a status of 200 and as json

--- controllers/animalCtrl.js
+++ controllers/animalCtrl.js
@@ -13,3 +13,13 @@ module.exports.getAnimals = (req, res, next) => {
     next(err);
   })
 };
+
+module.exports.getAnimal = ({params: {id}}, res, next) => {
+  Animal.getOneAnimal(id)
+  .then( (animal) => {
+    res.status(200).json(animal)
+  })
+  .catch( (err) => {
+    next(err)
+  })
+}
