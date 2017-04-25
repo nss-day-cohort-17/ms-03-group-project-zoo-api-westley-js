40cd9b967a19674897c05154a3ba46505c75cdb4
Jared Fuller <jaredshane@gmail.com>
2017-04-25 15:41:53 -0500

    added the delete route and required that function in

--- routes/animals.js
+++ routes/animals.js
@@ -2,12 +2,12 @@
 
 const { Router } = require('express');
 const router = Router();
-const { getAnimals, getAnimal, addAnimal } = require('../controllers/animalCtrl');
+const { getAnimals, getAnimal, addAnimal, deleteAnimal } = require('../controllers/animalCtrl');
 
 
 router.get('/animals', getAnimals);
 router.get('/animals/:id', getAnimal);
 router.post('/animals/new', addAnimal);
-
+router.delete('/animals/:id', deleteAnimal);
 
 module.exports = router;
