4da39c631a327de64ce0df9e42f7266090860f55
Jared Fuller <jaredshane@gmail.com>
2017-04-26 08:39:07 -0500

    required in updateAnimals and set up a route for a put request

--- routes/animals.js
+++ routes/animals.js
@@ -2,12 +2,14 @@
 
 const { Router } = require('express');
 const router = Router();
-const { getAnimals, getAnimal, addAnimal, deleteAnimal } = require('../controllers/animalCtrl');
+const { getAnimals, getAnimal, addAnimal, deleteAnimal, updateAnimals } = require('../controllers/animalCtrl');
 
 
 router.get('/animals', getAnimals);
 router.get('/animals/:id', getAnimal);
 router.post('/animals/new', addAnimal);
 router.delete('/animals/:id', deleteAnimal);
+router.put('/animals/:id', updateAnimals)
+
 
 module.exports = router;
