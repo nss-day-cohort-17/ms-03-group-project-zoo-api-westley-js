78d8fe49a476eaf4720fd33abbf771d948cf3efe
Jared Fuller <jaredshane@gmail.com>
2017-04-26 11:44:42 -0500

    added a route to get all the animals for a keeper

--- routes/keepers.js
+++ routes/keepers.js
@@ -4,8 +4,10 @@ const { Router } = require('express');
 const router = Router();
 
 const { getKeepers, getKeeper, addKeeper, deleteKeeper, updateKeeper } = require('../controllers/keeperCtrl')
+const { getAnimalsForKeeper } = require('../controllers/animal_keeperCtrl');
 
 router.get('/keepers', getKeepers);
+router.get('/keepers/animals/:id', getAnimalsForKeeper);
 router.get('/keepers/:id', getKeeper);
 router.post('/keepers/new', addKeeper);
 router.delete('/keepers/:id', deleteKeeper)
