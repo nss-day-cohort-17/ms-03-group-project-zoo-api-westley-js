2f7112fda787665c47ebb3a3496acc9640c45458
Sorrel Brigman <sorrel.k@gmail.com>
2017-04-27 09:48:56 -0500

    wrote table schema for trainers

--- db/migrations/20170427093900_trainers.js
+++ db/migrations/20170427093900_trainers.js
@@ -1,8 +1,19 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema
+    .table('trainers', function(table){
+      table.string('first_name').notNullable();
+      table.string('last_name').notNullable();
+      table.string('gender').notNullable();
+      table.string('age').notNullable();
+      table.string('date_of_hire').notNullable();
+      table.integer('category_id').notNullable().unsigned().references('categories.id')
+      table.string('trainer_image').notNullable();
+    });
 };
 
+
 exports.down = function(knex, Promise) {
-  
+  return knex.schema
+    .dropTableIfExists('trainers')
 };
